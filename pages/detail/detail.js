052becce32b2fa69f4eb053f9032a2addc942ca1
叶晨 <yechen_0@foxmail.com>
2026-04-17 14:50:15 +0800

    feat: 红粉色主题v0.1

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -2,8 +2,10 @@
 Page({
   data: {
     module: null,
-    columns: [[], []], // 瀑布流两列
-    media: []
+    media: [],
+    leftColumn: [],   // 左列 - 主要放竖图
+    rightColumn: [],  // 右列 - 主要放横图
+    imageInfoMap: {}  // 存储图片尺寸信息
   },
 
   onLoad(options) {
@@ -11,40 +13,42 @@ Page({
     var app = getApp()
     var modules = app.globalData.modules
     var moduleData = null
-    
+
     for (var i = 0; i < modules.length; i++) {
       if (modules[i].id === parseInt(options.moduleId)) {
         moduleData = modules[i]
         break
       }
     }
-    
+
     if (!moduleData) {
       console.error('Module not found')
       return
     }
-    
+
     var media = moduleData.media
     var processedMedia = []
-    
+
     // 预处理媒体数据
     for (var i = 0; i < media.length; i++) {
       var item = media[i]
       var isVideo = this.checkIsVideo(item)
-      var thumbnail = isVideo ? this.addVideoSnapshot(moduleData.cover) : item
-      
+      var thumbnail = isVideo ? this.addVideoSnapshot(item) : item
+
       processedMedia.push({
         url: item,
         isVideo: isVideo,
-        thumbnail: thumbnail
+        thumbnail: thumbnail,
+        originalIndex: i,
+        orientation: 'unknown' // 未知朝向，等待加载后判断
       })
     }
-    
+
     this.setData({
       module: moduleData,
       media: processedMedia
     })
-    
+
     // 初始化瀑布流布局
     this.initWaterfall()
   },
@@ -53,34 +57,83 @@ Page({
    * 检查是否为视频
    */
   checkIsVideo(url) {
-    var len = url.length;
-    var suffix = url.substring(len - 4, len);
-    return suffix === '.mp4';
+    var len = url.length
+    var suffix = url.substring(len - 4, len)
+    return suffix === '.mp4'
   },
 
   /**
    * 为视频URL添加截图参数显示第一帧
    */
   addVideoSnapshot(url) {
-    return url + '?x-oss-process=video/snapshot,t_0,f_jpg,m_fast';
+    return url + '?x-oss-process=video/snapshot,t_0,f_jpg,m_fast'
   },
 
   /**
    * 初始化瀑布流布局
+   * 初始时将所有媒体项分配到两列，保持平衡
    */
   initWaterfall() {
     var media = this.data.media
-    var columns = [[], []]
-    var columnIndex = 0
-    
-    // 简单的瀑布流布局，交替分配到两列
+    var leftCount = 0
+    var rightCount = 0
+
+    // 为每个媒体项分配列
     for (var i = 0; i < media.length; i++) {
-      columns[columnIndex].push(i)
-      columnIndex = (columnIndex + 1) % 2
+      var item = media[i]
+      
+      if (item.isVideo) {
+        item.orientation = 'landscape'
+      }
+      
+      // 分配到较短的列
+      if (leftCount <= rightCount) {
+        item.column = 'left'
+        leftCount++
+      } else {
+        item.column = 'right'
+        rightCount++
+      }
     }
-    
+
+    // 触发更新
     this.setData({
-      columns: columns
+      media: [...media]
+    })
+  },
+
+  /**
+   * 图片加载完成，获取尺寸信息并更新朝向
+   */
+  onImageLoad(e) {
+    var originalIndex = e.currentTarget.dataset.originalIndex
+    var width = e.detail.width
+    var height = e.detail.height
+
+    // 判断朝向：宽高比 > 1 为横图，<= 1 为竖图
+    var orientation = width > height ? 'landscape' : 'portrait'
+
+    // 更新图片信息
+    var imageInfoMap = this.data.imageInfoMap
+    imageInfoMap[originalIndex] = {
+      width: width,
+      height: height,
+      orientation: orientation,
+      loaded: true
+    }
+
+    // 更新媒体项的朝向
+    var media = this.data.media
+    for (var i = 0; i < media.length; i++) {
+      if (media[i].originalIndex === originalIndex) {
+        media[i].orientation = orientation
+        break
+      }
+    }
+
+    this.setData({
+      imageInfoMap: imageInfoMap,
+      media: [...media]
     })
   },
 
@@ -90,11 +143,11 @@ Page({
   previewMedia(e) {
     var index = e.currentTarget.dataset.index
     var mediaItem = this.data.media[index]
-    
+
     if (mediaItem.isVideo) {
       // 播放视频
       wx.navigateTo({
-        url: '/pages/video/video?url=' + mediaItem.url
+        url: '/pages/video/video?url=' + encodeURIComponent(mediaItem.url)
       })
     } else {
       // 预览图片
@@ -104,7 +157,7 @@ Page({
           imageUrls.push(this.data.media[i].url)
         }
       }
-      
+
       wx.previewImage({
         current: mediaItem.url,
         urls: imageUrls
@@ -113,10 +166,11 @@ Page({
   },
 
   onShow() {},
+
   onShareAppMessage() {
     return {
-      title: this.data.module.name + ' · 暖色Vlog',
-      path: '/pages/detail/detail?module=' + JSON.stringify(this.data.module)
+      title: this.data.module ? this.data.module.name + ' · 暖色Vlog' : '暖色Vlog',
+      path: '/pages/detail/detail?moduleId=' + (this.data.module ? this.data.module.id : '')
     }
   }
 })
--- pages/developing/developing.js
+++ pages/developing/developing.js
@@ -4,7 +4,12 @@ Page({
   },
 
   onLoad() {},
-  onShow() {},
+  onShow() {
+    // 更新 tabBar 选中状态
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({ active: 1 })
+    }
+  },
   onShareAppMessage() {
     return {
       title: '暖色Vlog · 生活记录',
